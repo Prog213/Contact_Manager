cb3674553209f7676408cf9eecaa45a0509fe4e1
Prog213 <notice457@gmail.com>
2025-01-10 22:07:23 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Contact_Manager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contact_Manager.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Contact> Contacts { get; set; } = null!;
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Xml.Linq;
+
+namespace Contact_Manager.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; } = null!;
+        public decimal Salary { get; set; }
+    }
+}
