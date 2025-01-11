4bea4778c77a63f8f8e92d74f5458e115f9c2d76
Prog213 <notice457@gmail.com>
2025-01-11 15:09:58 +0200

    Added list/delete contacts functionality

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,33 @@
+﻿using Contact_Manager.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contact_Manager.Controllers
+{
+    public class ContactsController(AppDbContext context) : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var contacts = await context.Contacts.ToListAsync();
+            return View(contacts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var contact = await context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            context.Contacts.Remove(contact);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(List));
+        }
+
+    }
+}
